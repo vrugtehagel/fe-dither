9617f3f7556a5a56ce5e08119dd0f23ba5590d5d
vrugtehagel <vrugtehagel@gmail.com>
2021-04-21 21:05:35 +0200

    added support for more intuitive resizing

--- index.js
+++ index.js
@@ -46,9 +46,6 @@ template.innerHTML = `
         #content {
             width: 100%;
             height: 100%;
-            position: absolute;
-            top: 0;
-            left: 0;
             background-color: inherit;
         }
 
